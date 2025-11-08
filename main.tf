da68603465dc7d213d0018e16f0d370310b1441b
Diego <diego_pkv@hotmail.com>
2025-11-08 19:58:39 +0100

    Add mysql container

--- main.tf
+++ main.tf
@@ -13,13 +13,30 @@ provider "docker" {}
 resource "docker_image" "nginx" {
   name = "nginx:latest"
 }
+# Crear una imagen de MySQL
+resource "docker_image" "mysql" {
+  name = "mysql:latest"
+}
 
-# Crear un contenedor a partir de la imagen
+# Crear un contenedor a partir de la imagen de MySQL
+resource "docker_container" "db" {
+  name  = var.db_name
+  image = docker_image.mysql.name
+  env = [ "MYSQL_ROOT_PASSWORD=${var.db_password}" ]
+  ports {
+    internal = 3306
+    external = var.db_port
+  }
+}
+
+# Crear un contenedor a partir de la imagen de Nginx
 resource "docker_container" "web" {
-  name  = var.name
+  name  = var.web_name
   image = docker_image.nginx.name
   ports {
     internal = 80
-    external = var.port
+    external = var.web_port
   }
+
+  depends_on = [docker_container.db]
 }
--- variables.tf
+++ variables.tf
@@ -1,9 +1,25 @@
-variable "name" {
+variable "db_name" {
+  type    = string
+  default = "db_server"
+}
+
+variable "db_password" {
+  type    = string
+  default = "root"
+  sensitive = true
+}
+
+variable "db_port" {
+  type    = number
+  default = 3306
+}
+
+variable "web_name" {
   type    = string
   default = "web_server"
 }
 
-variable "port" {
+variable "web_port" {
   type    = number
   default = 8080
 }
